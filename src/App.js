32f6d729c1d925a544dc972347c9677f917d5b86
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-01-03 14:58:43 -0500

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './';
+import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar.js';
 import { Banner } from './components/Banner.js';
 import { Skills } from './components/Skills.js';
 import { Projects } from './components/Projects.js';
 import { Contact } from './components/Contact.js';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
